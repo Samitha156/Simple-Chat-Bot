5d952227a96b2648b590bce45f8218f80c5baafb
Samitha Parinda Randeniya <samitha156@gmail.com>
2021-06-17 01:37:22 -0400

    Add files via upload

---
+++ chat_bot.py
@@ -0,0 +1,64 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import re 
+import long_responses as long
+import sys
+
+
+# In[4]:
+
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words =True
+    
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty +=1
+            
+    percentage = float(message_certainty)/float(len(recognised_words))
+    
+    
+    for words in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+            
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else:
+        return 0
+def check_all_messages(message):
+    highest_prob_list = {}
+    
+    def response(bot_response, list_of_words, single_response = False, required_words =[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+        
+    ###REsponse--------
+    response('Hello!',['hello','hi','sup','hey','heyo'], single_response = True)
+    response('I\'m doing well, and you?',['how','are','you','doing'], required_words=['how'])
+    response('Thank you!',['i','love','code','palace'],required_words=['code','palace'])
+    response(long.R_EATING,['what','you','eat'], required_words = ['you','eat'])
+    
+    best_match = max(highest_prob_list, key= highest_prob_list.get)
+    print(highest_prob_list)
+    
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+def get_response(user_input):
+    if user_input == 'exit':
+        return sys.exit()
+    else:
+        
+        split_message = re.split(r'\s+|[,;?!.-]\s*',user_input.lower())
+        response = check_all_messages(split_message)
+        return response
+
+while True:
+    print('Bot:' + get_response(input('You: ')))
+
